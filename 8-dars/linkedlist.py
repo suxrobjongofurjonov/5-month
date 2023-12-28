42dafaef4af72e46f477e1e79ec5975584bb01ba
odiljonov04 <115708685+odiljonov04@users.noreply.github.com>
2023-12-28 17:32:02 +0500

    8-dars

---
+++ 8-dars/linkedlist.py
@@ -0,0 +1,57 @@
+### context manager ###
+# with open('sample.txt','w') as file:
+#     file.write('Hello world')
+
+# class OpenFile:
+#     def __init__(self, filename, mode) -> None:
+#         self.filename=filename
+#         self.mode=mode
+        
+#     def __enter__(self):
+#         self.file=open(self.filename, self.mode)
+#         return self.file
+    
+#     def __exit__(self, exc_t, axc_v, traceback):
+#         self.file.close()
+        
+# with OpenFile('sample.txt','w') as file:
+#     file.write('Salom, Dunyo')
+    
+# print(file.closed)
+
+# list_a=[5,8,12,7,15,79]
+# list_a.remove(8)
+# print(list_a)
+### linked list ###
+# class Node:
+#     def __init__(self, data=None) -> None:
+#         self.data=data
+#         self.next=None
+        
+# class LinkedList:
+#     def __init__(self) -> None:
+#         self.head=Node()
+        
+#     def append(self, data):
+#         new_node=Node(data)
+#         current=self.head
+        
+#         while current.next!=None:
+#             current=current.next
+#         current.next=new_node
+        
+#     def show(self):
+#         current=self.head
+#         elements=[]
+        
+            
+#         while current.next!=None:
+#             current=current.next
+#             elements.append(current.data)
+#         return elements
+    
+# my_l=LinkedList()
+# my_l.append(2)
+# my_l.append(1)
+# my_l.append(4)
+# print(my_l.show())
