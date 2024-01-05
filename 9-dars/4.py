d82dddf13d76fca600de29aa305aef719c64dfc7
odiljonov04 <115708685+odiljonov04@users.noreply.github.com>
2024-01-05 23:47:36 +0500

    4mas

--- 9-dars/4.py
+++ 9-dars/4.py
@@ -1,10 +1,13 @@
-def func(text1, text2):
-    farq=[]
-    for i in set(text1+text2):
-        if text1.count(i) != text2.count(i):
-            farq.append(i)
-    return farq
-s=""
-t="y"
+import random
+def func(s, t):
+    s=t[:]
+    tasodifiy=random.choice(s)
+    t+= tasodifiy
+    
+    return tasodifiy
 
-print(func(s,t))
+s = ""
+t = "y"
+
+words=func(s, t)
+print(words)
